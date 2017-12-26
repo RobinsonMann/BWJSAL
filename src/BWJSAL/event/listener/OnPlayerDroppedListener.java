45707e8d3ce4c3e277ebc9795f725081b649b5bc
robinsonmann <Robinson.Mann@uwaterloo.ca>
2017-12-26 14:22:05 -0500

    Added comment and method argument to OnPlayerDroppedListener interface

--- src/BWJSAL/event/listener/OnPlayerDroppedListener.java
+++ src/BWJSAL/event/listener/OnPlayerDroppedListener.java
@@ -1,9 +1,15 @@
 package BWJSAL.event.listener;
 
+import bwapi.Player;
+
 /**
  * The listener interface for receiving notification of game onPlayerDropped events.
  * Any classes that are interested in monitoring onPlayerDropped events should implement this interface.
  */
 public interface OnPlayerDroppedListener extends GameEventListener {
-    void onPlayerDropped();
+    /**
+     * Called when a Player is dropped from the game.
+     * @param player The Player interface object representing the player that dropped from the game.
+     */
+    void onPlayerDropped(Player player);
 }
