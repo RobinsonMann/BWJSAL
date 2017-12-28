05d4b4ae0c01404035748ec98efb9915cf90f5e4
robinsonmann <Robinson.Mann@uwaterloo.ca>
2017-12-28 11:48:32 -0500

    Information class that tracks build time of UnitType

---
+++ src/BWJSAL/Information/BuildTimeTracker.java
@@ -0,0 +1,109 @@
+package BWJSAL.Information;
+
+import BWJSAL.event.listener.OnStartListener;
+import bwapi.Game;
+import bwapi.Race;
+import bwapi.Unit;
+import bwapi.UnitType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Based on information observed, tracks what unit types the enemy has produced
+ * and the earliest time that the unit type was observed.
+ */
+public class BuildTimeTracker implements OnStartListener {
+    private final Game game;
+    private final Map<UnitType, Integer> earliestBuildTime;
+
+    public BuildTimeTracker(final Game game) {
+        this.game = game;
+        this.earliestBuildTime = new HashMap<>();
+    }
+
+    @Override
+    public void onStart() {
+        buildTimesForStartingUnits();
+    }
+
+    /**
+     * Retrieve if it has been observed that an enemy has built this unit type, or it can infer such from that player's tech tree.
+     *
+     * @param unitType UnitType to check if the enemy has built
+     * @return true if our observations confirms that the enemy has built this unit.
+     */
+    public boolean enemyHasBuilt(final UnitType unitType) {
+        return this.earliestBuildTime.containsKey(unitType);
+    }
+
+    /**
+     * From what has been observed, infer the latest possible time that the enemy must have build this unit type.
+     * Latest possible build time is determined from direct observation or inferred from that tech required for another
+     * observed unit.
+     *
+     * @param unitType The UnitType to get inferred latest possible build time for.
+     * @return Time (as frame count) when we first discovered that the enemy has constructed the given UnitType.
+     */
+    public Integer getBuildTime(final UnitType unitType) {
+        if (enemyHasBuilt(unitType) == false) {
+            throw new IllegalArgumentException(String.format("Enemy has not built unitType %s", unitType.toString()));
+        }
+
+        return this.earliestBuildTime.get(unitType);
+    }
+
+    /**
+     * Mark that we have observed the given UnitType at the current time.
+     * This will recursively update the BuildTime for all units along the units tech tree.
+     */
+    private void updateBuildTime(final UnitType unitType, final int time) {
+        if (this.earliestBuildTime.containsKey(unitType)) {
+            final int previouslyObservedBuiltTime = this.earliestBuildTime.get(unitType);
+
+            if (previouslyObservedBuiltTime <= time || previouslyObservedBuiltTime == 0) {
+                return;
+            }
+        }
+
+        this.earliestBuildTime.put(unitType, time);
+
+        // Sanity check. (Present inside of original BWSAL)
+        if (time < 0) {
+            return;
+        }
+
+        // Update earliest known build times of required units
+        for (final UnitType techTreeParent : unitType.requiredUnits()
+                                                     .keySet()) {
+            final int latestPossibleBuildTimeOfParentTechType = time - techTreeParent.buildTime();
+            updateBuildTime(techTreeParent, latestPossibleBuildTimeOfParentTechType);
+        }
+    }
+
+    /**
+     * At the start of every game, based off of the enemies race, we know the build times for certain
+     * units / items.
+     */
+    private void buildTimesForStartingUnits() {
+        final Race enemyRace = this.game.enemy()
+                                        .getRace();
+
+        // Regardless of race, enemy will start with workers and command center
+        updateBuildTime(enemyRace.getCenter(), 0);
+        updateBuildTime(enemyRace.getWorker(), 0);
+
+        // We know some additional
+        if (enemyRace == Race.Zerg) {
+            updateBuildTime(UnitType.Zerg_Overlord, 0);
+            updateBuildTime(UnitType.Zerg_Larva, 0);
+        }
+    }
+
+    public void onUnitDiscover(final Unit unit, final Integer time) {
+        if (this.game.self().isEnemy(unit.getPlayer())) {
+            final UnitType unitType = unit.getType();
+            updateBuildTime(unitType, time - unitType.buildTime());
+        }
+    }
+}
---
+++ tst/BWJSAL/Information/BuildTimeTrackerTest.java
@@ -0,0 +1,202 @@
+package BWJSAL.Information;
+
+import BWJSAL.utils.UnitBuilder;
+import bwapi.Game;
+import bwapi.Player;
+import bwapi.Race;
+import bwapi.UnitType;
+import org.assertj.core.util.Maps;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static BWJSAL.utils.StaticFinalUtils.setStaticFinalField;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
+
+public class BuildTimeTrackerTest {
+    private Game mockGame;
+    private Player mockSelf;
+    private Player mockEnemy;
+    private BuildTimeTracker target;
+
+    @Before
+    public void setUp() throws Exception {
+        this.mockGame = Mockito.mock(Game.class);
+        this.mockSelf = Mockito.mock(Player.class);
+        this.mockEnemy = Mockito.mock(Player.class);
+        this.target = new BuildTimeTracker(this.mockGame);
+
+        when(this.mockGame.enemy()).thenReturn(this.mockEnemy);
+        when(this.mockGame.self()).thenReturn(this.mockSelf);
+        when(this.mockSelf.isEnemy(this.mockEnemy)).thenReturn(true);
+
+        setupUnitTypes();
+    }
+
+    @Test
+    public void enemyHasBuilt_onStart_haveNotBuiltNonStartingUnit() {
+        setEnemyRace(createMockRace(UnitType.Terran_Command_Center, UnitType.Terran_SCV));
+
+        this.target.onStart();
+
+        assertThat(this.target.enemyHasBuilt(UnitType.Terran_Marine)).isFalse();
+    }
+
+    @Test
+    public void enemyHasBuilt_enemyZerg_additionalStartingUnitTypes() throws Exception {
+        final Race mockZerg = createMockRace(UnitType.Zerg_Hatchery, UnitType.Zerg_Drone);
+        setStaticFinalField(Race.class, "Zerg", mockZerg);
+
+        when(this.mockGame.self().isEnemy(this.mockEnemy)).thenReturn(true);
+        when(this.mockEnemy.getRace()).thenReturn(Race.Zerg);
+
+        this.target.onStart();
+
+        assertEnemyHasBuilt(UnitType.Zerg_Overlord, "Zerg starts with overlord");
+        assertEnemyHasBuilt(UnitType.Zerg_Larva, "Zerg starts with larva");
+    }
+
+    @Test
+    public void enemyHasBuilt_mockEnemyRace_hasBuiltForCenterAndWorkersTrue() throws Exception {
+        final UnitType mockCenter = Mockito.mock(UnitType.class);
+        final UnitType mockWorker = Mockito.mock(UnitType.class);
+        final Race mockRace = createMockRace(mockCenter, mockWorker);
+
+        when(this.mockEnemy.getRace()).thenReturn(mockRace);
+
+        this.target.onStart();
+
+        assertEnemyHasBuilt(mockCenter, "Race's Center UnitType always built");
+        assertEnemyHasBuilt(mockWorker, "Race's Worker UnitType always built");
+    }
+
+    /**
+     * Update build time
+     */
+    @Test
+    public void onUnitDiscovery_requiredUnitsNotPreviouslyObserved_requiredUnitBuildTimesSetCorrectly() {
+        final int currentGameTime = 1000;
+        final int dragoonBuildTime = 50;
+        final int cyberneticsCoreBuildTime = 100;
+        final int gatewayBuildTime = 100;
+
+        when(UnitType.Protoss_Dragoon.buildTime()).thenReturn(dragoonBuildTime);
+        when(UnitType.Protoss_Cybernetics_Core.buildTime()).thenReturn(cyberneticsCoreBuildTime);
+        when(UnitType.Protoss_Gateway.buildTime()).thenReturn(gatewayBuildTime);
+
+        this.target.onUnitDiscover(UnitBuilder.mockUnit(UnitType.Protoss_Dragoon, this.mockEnemy), currentGameTime);
+
+        assertUnitTypeBuildTime(UnitType.Protoss_Dragoon,
+                                currentGameTime - dragoonBuildTime,
+                                "Latest build time for top unit is game time - build time");
+
+        assertUnitTypeBuildTime(UnitType.Protoss_Cybernetics_Core,
+                                currentGameTime - dragoonBuildTime - cyberneticsCoreBuildTime,
+                                "Latest build time recursively called for required units");
+
+        assertUnitTypeBuildTime(UnitType.Protoss_Gateway,
+                                currentGameTime - dragoonBuildTime - cyberneticsCoreBuildTime - gatewayBuildTime,
+                                "Latest build time recursively called for required units");
+    }
+
+    @Test
+    public void onUnitDiscovery_previouslyObservedUnit_observedBuildTimeNewer_buildTimeNotUpdated() {
+        final int firstGameTime = 1000;
+        final int secondGameTime = 10000;
+        this.target.onUnitDiscover(UnitBuilder.mockUnit(UnitType.Protoss_Gateway, this.mockEnemy), firstGameTime);
+        this.target.onUnitDiscover(UnitBuilder.mockUnit(UnitType.Protoss_Gateway, this.mockEnemy), secondGameTime);
+
+        assertUnitTypeBuildTime(UnitType.Protoss_Gateway,
+                                firstGameTime,
+                                "build time not changed");
+    }
+
+    @Test
+    public void onUnitDiscovery_observeStartingUnit_buildTimeNotUpdated() {
+        setEnemyRace(createMockRace(UnitType.Terran_Command_Center, UnitType.Terran_SCV));
+        this.target.onStart();
+
+        this.target.onUnitDiscover(UnitBuilder.mockUnit(UnitType.Terran_SCV, this.mockEnemy), 10);
+
+        assertUnitTypeBuildTime(UnitType.Terran_SCV,
+                                0,
+                                "build time not changed");
+    }
+
+    @Test
+    public void getBuildTime_onStart_startingUnitsBuildTimeOf0() {
+        final Race mockZerg = createMockRace(UnitType.Zerg_Hatchery, UnitType.Zerg_Drone);
+        setStaticFinalField(Race.class, "Zerg", mockZerg);
+
+        when(this.mockEnemy.getRace()).thenReturn(Race.Zerg);
+
+        this.target.onStart();
+
+        assertEnemyHasBuilt(UnitType.Zerg_Overlord, "Zerg starts with overlord");
+        assertEnemyHasBuilt(UnitType.Zerg_Larva, "Zerg starts with larva");
+    }
+
+    @Test
+    public void getBuildTime_unitNotBuilt() {
+        setupUnitTypes();
+        assertThatThrownBy(() -> this.target.getBuildTime(UnitType.Protoss_Dragoon))
+                .hasMessage("Enemy has not built unitType Protoss_Dragoon");
+    }
+
+    private Race createMockRace(final UnitType center, final UnitType worker) {
+        final Race mockRace = Mockito.mock(Race.class);
+        when(mockRace.getCenter()).thenReturn(center);
+        when(mockRace.getWorker()).thenReturn(worker);
+        return mockRace;
+    }
+
+    private void setEnemyRace(final Race race) {
+        when(this.mockEnemy.getRace()).thenReturn(race);
+    }
+
+    /**
+     * Properties of unit types are loaded from DLLS on game start.
+     * As the unit tests don't start the the game, we need to mock
+     * the final constant unit types.
+     */
+    private void setupUnitTypes() {
+        modifyUnitTypeToMock("Terran_SCV", new HashMap<>());
+        modifyUnitTypeToMock("Terran_Command_Center", new HashMap<>());
+
+        modifyUnitTypeToMock("Zerg_Drone", new HashMap<>());
+        modifyUnitTypeToMock("Zerg_Hatchery", new HashMap<>());
+        modifyUnitTypeToMock("Zerg_Larva", new HashMap<>());
+        modifyUnitTypeToMock("Zerg_Overlord", new HashMap<>());
+
+        modifyUnitTypeToMock("Protoss_Nexus", new HashMap<>());
+        modifyUnitTypeToMock("Protoss_Pylon", new HashMap<>());
+        modifyUnitTypeToMock("Protoss_Gateway", Maps.newHashMap(UnitType.Protoss_Pylon, 1));
+        modifyUnitTypeToMock("Protoss_Cybernetics_Core", Maps.newHashMap(UnitType.Protoss_Gateway, 1));
+        modifyUnitTypeToMock("Protoss_Dragoon", Maps.newHashMap(UnitType.Protoss_Cybernetics_Core, 1));
+    }
+
+    private void modifyUnitTypeToMock(final String unitTypeName,
+                                      final Map<UnitType, Integer> requiredUnits) {
+        final UnitType mockDrone = Mockito.mock(UnitType.class);
+        when(mockDrone.toString()).thenReturn(unitTypeName);
+        when(mockDrone.requiredUnits()).thenReturn(requiredUnits);
+        setStaticFinalField(UnitType.class, unitTypeName, mockDrone);
+    }
+
+    private void assertEnemyHasBuilt(final UnitType unitType, final String description) {
+        assertThat(this.target.enemyHasBuilt(unitType))
+                .as(description)
+                .isTrue();
+    }
+
+    private void assertUnitTypeBuildTime(final UnitType unitType, final Integer expectedBuildTime, final String description) {
+        assertThat(this.target.getBuildTime(unitType))
+                .as(description)
+                .isEqualTo(expectedBuildTime);
+    }
+}
